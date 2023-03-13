bfed19e6471e024ca7ef99677fa2f3ebd180d491
alanb-02 <mathew02alan@gmail.com>
2023-03-13 14:51:50 +0000

    creation of the products page based on template created earlier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import data from './data';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="grid-container">
+            <header class="row">
+                <div>
+                    <a class="brandName" href="index.html">Sight2See</a>
+                </div>
+                <div>
+                    <a href="cart.html">Cart</a>
+                    <a href="signIn.html">Sigh In</a>
+                </div>
+            </header>
+
+            <main>
+                <div class="row center">
+                    {
+                        data.products.map(product => (
+                            <div key={product.id} class="card">
+                                <a href={`/product/${product._id}`}>{/* this is telling the link for the product of that specific product id */}
+                                    <img class="medium" src={product.image} alt={product.name}/>{/* image size must be: 680px by 830px */}
+                                </a>
+                                <div class="card-body">
+                                    <a href={`/product/${product._id}`}>{/* this is telling the link for the product of that specific product id */}
+                                        <h2>{product.name}</h2>
+                                        <h3>{product.brand}</h3>
+                                    </a>
+                                    <div class="rating">
+                                        <span><i class="fa fa-star"></i></span>
+                                        <span><i class="fa fa-star"></i></span>
+                                        <span><i class="fa fa-star"></i></span>
+                                        <span><i class="fa fa-star"></i></span>
+                                        <span><i class="fa fa-star"></i></span>
+                                    </div>
+                                    <div class="price">
+                                        ${product.price}
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </main>
+            
+            <footer class="row center">
+                By Alan Byju
+            </footer>
+        </div>
   );
 }
 
---
+++ frontend/src/data.js
@@ -0,0 +1,93 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Mens Frame 1',
+            category: 'men',
+            image: 'images/p1.jpg',
+            price: 125,
+            brand: 'Hugo',
+            rating: 4.0,
+            numReviews: 10,
+            desciption: "Upgrade your style with these sleek and durable mens glasses, featuring a classic design and comfortable fit.",
+        },
+
+        {
+            _id: '2',
+            name: 'Womens Frame 1',
+            category: 'women',
+            image: 'images/p2.jpg',
+            price: 150,
+            brand: 'Gucci',
+            rating: 4.5,
+            numReviews: 20,
+            desciption: "Elevate your look with these sophisticated and feminine women's glasses, designed with a modern aesthetic and premium materials.",
+        },
+        
+        {
+            _id: '3',
+            name: 'Kids Frame 1',
+            category: 'child',
+            image: 'images/p3.jpg',
+            price: 85,
+            brand: 'Disney',
+            rating: 5.0,
+            numReviews: 15,
+            desciption: "Get your little one ready for school with these fun and durable kids glasses, designed for a comfortable fit and with a colorful frame.",
+        },
+
+        {
+            _id: '4',
+            name: 'Mens Frame 2',
+            category: 'men',
+            image: 'images/p1.jpg',
+            price: 200,
+            brand: 'Diesel',
+            rating: 3.5,
+            numReviews: 5,
+            desciption: "Refreshing new look for men with glossy black plastic and grey metal accents.",
+        },
+
+        {
+            _id: '5',
+            name: 'Womens Frame 2',
+            category: 'women',
+            image: 'images/p2.jpg',
+            price: 125,
+            brand: 'Coach',
+            rating: 4.0,
+            numReviews: 12,
+            desciption: "Make a statement with these trendy women's glasses, featuring a unique frame design and high-quality lenses for optimal vision."
+
+
+
+            ,
+        },
+
+        {
+            _id: '6',
+            name: 'Kids Frame 2',
+            category: 'child',
+            image: 'images/p3.jpg',
+            price: 75,
+            brand: 'Marvel',
+            rating: 4.0,
+            numReviews: 2,
+            desciption: "Keep your child seeing clearly and looking cute with these fun and durable child-sized glasses.",
+        },
+
+        {
+            _id: '7',
+            name: 'Mens Frame 3',
+            category: 'men',
+            image: 'images/p1.jpg',
+            price: 250,
+            brand: 'Tommy',
+            rating: 4.5,
+            numReviews: 25,
+            desciption: "Upgrade your eyewear game with these stylish and durable men's glasses, perfect for both work and play.",
+        },
+    ]
+}
+
+export default data
