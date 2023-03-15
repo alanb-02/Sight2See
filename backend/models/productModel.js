d9f870a39f7e134cb3d415f6f08f02e92c507754
alanb-02 <mathew02alan@gmail.com>
2023-03-15 18:06:49 +0000

    added the products to the database and fetched the protucts from it onto the app -update

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -5,6 +5,7 @@ const productSchema = new mongoose.Schema(
     name: { type: String, required: true, unique: true },
     slug: { type: String, required: true, unique: true },
     image: { type: String, required: true },
+    threeD: { type: String, required: true },
     brand: { type: String, required: true },
     category: { type: String, required: true },
     description: { type: String, required: true },
